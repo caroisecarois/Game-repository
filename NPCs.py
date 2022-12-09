28bae6a3f7846b24cdd206dfb93efd18cff13063
caroisecarois <carloscallejarstudent.ie.edu>
2022-12-09 11:48:06 +0100

    Please add all NPC's on this file following the template eexcept the dragon

---
+++ NPCs.py
@@ -0,0 +1,7 @@
+#Define the npc Ayara Orinelis, the elf queen
+class elfqueen():
+    def __init__(self):
+        self.name= "Ayara Orinelis"
+        self.type= "Elf queen"
+        self.stats= {"Strength": 8, "Dexterity": 12 ,"Intelligence": 16,"Perception": 12,
+                      "Charisma": 14,"Defense": 14}
